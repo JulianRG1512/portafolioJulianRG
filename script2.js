101f8c561c6fb0d49fea82230f1468bce54c87f2
JulianRG <julianrg1512@gmail.com>
2023-11-01 15:28:02 -0500

    Creacion Proyecto

---
+++ script2.js
@@ -0,0 +1,55 @@
+
+(function() {
+    $('.btn4').click(function() {
+        $(this).toggleClass('active');
+        return $('.box4') .toggleClass('open4');
+    });
+    
+}).call(this);
+
+(function() {
+    $('.btn4').click(function() {
+        $(this).toggleClass('active');
+        return $('.box4_1') .toggleClass('open4');
+    });
+    
+}).call(this);
+
+
+(function() {
+    $('.btn2').click(function() {
+        $(this).toggleClass('active');
+        return $('.box2').toggleClass('open2');
+    });
+    
+}).call(this);
+
+
+
+(function() {
+    $('.btn3').click(function() {
+        $(this).toggleClass('active');
+        return $('.box3').toggleClass('open3');
+    });
+    
+}).call(this);
+
+
+
+
+(function() {
+    $('.btn5').click(function() {
+        $(this).toggleClass('active');
+        return $('.box5') .toggleClass('open5');
+    });
+    
+}).call(this);
+
+(function() {
+    $('.btn5').click(function() {
+        $(this).toggleClass('active');
+        return $('.box5_1') .toggleClass('open5');
+    });
+    
+}).call(this);
+
